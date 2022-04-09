c60e61234924fedacaaf88d63c5544361c64c547
Razib Saha <rajibpay@gmail.com>
2022-04-09 12:37:02 +0600

    firebase private route

---
+++ src/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyANs8d7gqp6dS4qZ-5W1EwiR3bL2Sk_Zts",
+  authDomain: "private-route-37f2d.firebaseapp.com",
+  projectId: "private-route-37f2d",
+  storageBucket: "private-route-37f2d.appspot.com",
+  messagingSenderId: "491279966987",
+  appId: "1:491279966987:web:03ba4d72d2fcb5ee08f80e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
